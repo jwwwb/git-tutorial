9da5e893523482cb9e041dba51f6c231dade46de
James W. Browne <james.browne@joblocal.de>
2019-04-25 10:23:31 +0200

    Adds goodbye

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
-def hello:
+def goodbye():
+    print("Goodbye, World!")
+
+def hello():
     print("Hello, World!")
+
+def thanks():
+    print("Thanks, World!")
 
 if __name__ == '__main__':
     hello()
